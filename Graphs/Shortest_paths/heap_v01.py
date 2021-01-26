7db075363ce91ad5ca9d888e960b5cbda3e9dd23
SelferRy <vlselfy@mail.ru>
2021-01-26 19:59:09 +0300

    Compare with work functions.
    Heap-constructor v00.

--- Graphs/Shortest_paths/heap_v01.py
+++ Graphs/Shortest_paths/heap_v01.py
@@ -2,9 +2,9 @@
 
 class Heap(object):
 
-    def __init__(self, array):
-        self.array = array
-        self._hs = None
+    # def __init__(self, array):
+    #     self.array = array
+    #     self._hs = None
 
     def parent(self, i):
         return i // 2
@@ -15,19 +15,19 @@ class Heap(object):
     def right(self, i):
         return 2*i + 1
 
-    @property
-    def heap_size(self):
-        if self._hs == None:
-            self.heap_size = len(self.array) - 1
-        return self._hs
+    # @property
+    # def heap_size(self):
+    #     if self._hs == None:
+    #         self.heap_size = len(self.array)
+    #     return self._hs
+    #
+    # @heap_size.setter
+    # def heap_size(self, i):
+    #     self._hs = i
+    #     return self._hs
 
-    @heap_size.setter
-    def heap_size(self, i):
-        self._hs = i
-        return self._hs
 
-
-    def max_heapify(self, i):
+    def max_heapify(self, A, i):
         A = Heap(self.array) # self.array # self.Instance.heap_size(A)
         l = self.left(i)
         r = self.right(i)
@@ -41,35 +41,28 @@ class Heap(object):
             A.array[i], A.array[largest] = A.array[largest], A.array[i]
             self.max_heapify(largest)
 
-    def min_heapify(self, i):
+    def min_heapify(self, A, i):
         A = Heap(self.array) # self.array # self.Instance.heap_size(A)
         l = self.left(i)
         r = self.right(i)
-        if l <= A.heap_size and A.array[l] < A.array[i]:
-            smallest = l
+        heap_size = len(self.array) - 1
+        if l < heap_size and A.array[l] < A.array[i]:
+            smaller = l
         else:
-            smallest = i
-        if r <= A.heap_size and A.array[r] < A.array[smallest]:
-            smallest = r
-        if smallest != i:
-            A.array[i], A.array[smallest] = A.array[smallest], A.array[i]
-            self.min_heapify(smallest)
+            smaller = i
+        if r < heap_size and A.array[r] < A.array[smaller]:
+            smaller = r
+            A.array[i], A.array[smaller] = A.array[smaller], A.array[i]
+            self.min_heapify(smaller)
 
     def build_max_heap(self):
         self.heap_size = len(self.array)
         for i in range(len(self.array)//2, 0, -1):
             self.max_heapify(i)
 
-    def build_min_heap(self):
-        for i in reversed(range(len(self.array)//2)):
-            self.min_heapify(i)
-
-
 
 
 if __name__ == "__main__":
     A = Heap([10, 50, 3, 8, 2])
-    A.build_max_heap()
-    print(A.array)
-    A.build_min_heap()
-    print(A.array)
+    test = A.build_max_heap()
+    print(test)
