39724fb5f49101f2f350de47293f568dbd41e851
Vladimir Selifanov <vlselfy@mail.ru>
2021-03-27 23:33:10 +0300

    2-sum step 3.
    Find bug in 'two_sums.py' (why count was so mach larger?). Save before fix.

--- Two_sum/tsum.py
+++ Two_sum/tsum.py
@@ -1,19 +1,13 @@
 def TwoSum_HashTable(lst, hashTable, target):
-    '''
+    """
     2-SUM algorithm via hash table.
 
     O(n) time.
-    '''
-
-    # hashTable = dict()
-
-    # for x in lst:
-    #     hashTable[x] = True
-
+    """
     for x in lst:
         y = target - x
         if y in hashTable and x != y:
-            return (x, y)
+            return x, y
 
 
 if __name__ == "__main__":
@@ -22,10 +16,15 @@ if __name__ == "__main__":
     data = list(map(int, data[:-1]))
     hashtable = {x: True for x in data}
 
+    lst = []
     count = 0
     for t in range(-10000, 10001):
-        if TwoSum_HashTable(data, hashtable, t):
+        marker = TwoSum_HashTable(data, hashtable, t)
+        if marker:
             count += 1
-        if count % 50 == 0:
-            print("count =", count)
+            lst.append(marker)
+            print(marker, f"x + y = {sum(marker)}")
+
+        # if count % 50 == 0:
+        #     print("count =", count)
     print('Via hash table: ' + str(count))
--- Two_sum/two_sums.py
+++ Two_sum/two_sums.py
@@ -1,14 +1,33 @@
-file = open("task3_2sum.txt", "r")
-array = file.read().split("\n")  # # file.readlines() # [line.split("\n") for line in file.readlines()]
-array = list(map(int, array[:-1]))
+def twosum_hashtable(t, x):
+    global count
+    global lst
+    global hashtable
+    y = t - x
+    if y in hashtable and x != y:
+        count += 1
+        lst.append((x, y))
+        return x, y
 
-hashtable = {x: x for x in array}
-lst = []
-count = 0
-for x in array:
+
+if __name__ == "__main__":
+    file = open("task3_2sum.txt", "r")
+    array = file.read().split("\n")  # # file.readlines() # [line.split("\n") for line in file.readlines()]
+    array = list(map(int, array[:-1]))
+    hashtable = {x: x for x in array}
+
+    lst = []
+    count = 0
     for t in range(-10000, 10001):
-        y = t - x
-        if y in hashtable and x != y:
-            count += 1
-        if count % 50 == 0:
-            print(count, x, y, x + y, sep="\n", end="\n" * 2)
+        for x in array:
+            mark = twosum_hashtable(t, x)
+            if mark:
+                print(mark, sum(mark), f"count = {count}")
+
+        # progress-bar
+        stage = t % int(1 / 10 * 10**4)
+        print(f"stage : {stage}")
+        # if stage == 0:
+        #     print(f"count = {count}")
+
+
+                # print(lst[stage - 5: stage + 1])
